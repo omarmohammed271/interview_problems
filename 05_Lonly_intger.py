ed1cee078ad688a88e70e65210083348830f327e
moooooora <omarmohammed271@gmail.com>
2024-06-11 11:43:06 +0300

    Added Lonly Integer

---
+++ 05_Lonly_intger.py
@@ -0,0 +1,9 @@
+def lonelyinteger(a:list):
+    # Write your code here
+    n = len(a)
+    
+    for i in a:
+        if a.count(i) == 1:
+            return i
+
+print(lonelyinteger([1,2,3,4,3,2,1,4,6]))     
